86dbd07fd33bdc138c14cc0216fe0736be80fb68
Askarbekov Aktan <akn392204@gmail.com>
2024-05-30 19:34:25 +0600

    Add Get request with query of id and name

--- Controllers/CountryController.cs
+++ Controllers/CountryController.cs
@@ -19,5 +19,22 @@ public class CountryController : Controller
     {
         return await _context.Countries.ToListAsync();
     }
-
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Country>> Get(int id)
+    {
+        Country? country = await _context.Countries.FirstOrDefaultAsync(c => c.Id == id);
+        if (country == null)
+            return NotFound();
+        return new ObjectResult(country);
+    }
+    
+    [HttpGet("{name}")]
+    public async Task<ActionResult<Country>> Get(string name)
+    {
+        Country? country = await _context.Countries.FirstOrDefaultAsync(c => c.Name.ToLower() == name.ToLower() || c.OfficialName.ToLower() == name.ToLower());
+        if (country == null)
+            return NotFound();
+        return new ObjectResult(country);
+    }
 }
